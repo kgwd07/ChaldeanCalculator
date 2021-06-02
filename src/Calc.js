4e5e41534c647c6491b2b9cabe3a6f75c7486413
kiran <kgwd07@gmail.com>
2021-06-02 14:22:36 +0530

    added clear button and updated UI

--- src/Calc.js
+++ src/Calc.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {useState} from 'react';
 import Chaldean from './Chaldean';
 import PeopleComp from './PeopleComp';
+import logo from './LNH-LOGO.png'
 
  const Calc = () => {
 
     const [firstName,setFirstName] = useState('');
     const [people,setPeople] = useState([]);
-    
+    const [isError,setIsError] = useState(false);
     const handleSubmit = (e) => {
         e.preventDefault();
         if(firstName){
@@ -26,19 +27,31 @@ import PeopleComp from './PeopleComp';
        }
     }
 
+    const clear = () => {
+        setPeople((people) => {
+           return [];
+       })
+    }
     
 
     return (
         <>
         <article>
-            
+            <div className="logo-container">
+                <img src={logo} alt="" />
+            </div>
+            <h3>CHALDEAN CALCULATOR</h3>
             <form className='form' onSubmit = {handleSubmit}> 
+                
                 <div className="form-control">
+                    
                     <label htmlFor="firstName">NAME : </label>
-                        <input type="text" id='firstName' name='firstName' value = {firstName} onChange = {(e)=> setFirstName( e.target.value)} required minLength='3' maxLength='100' pattern = '^[a-zA-Z]*$'/>
+                        <input type="text" id='firstName' name='firstName' value = {firstName} onChange = {(e)=> setFirstName( e.target.value)} required minLength='3' maxLength='100' pattern = '^[a-zA-Z]*$' placeholder='enter name'/>
                 </div>
-
+                <div className="button-container">
                 <button type='submit'>find no</button>
+                <button type='btn' onClick={clear}>clear list</button>
+                </div>
             </form>
 
             { people.map((person) => {
